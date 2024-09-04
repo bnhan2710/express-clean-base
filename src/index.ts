080026b0e71c51a016f607bb7526e5cafd6f73a0
Huỳnh Như Bảo Nhân <baonhannice@gmail.com>
2024-09-04 10:17:34 +0700

    initial project

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express , {Express , Request , Response} from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app:Express = express();
+const PORT = process.env.PORT || 8000;
+
+app.get('/', (req:Request , res:Response) => {
+    res.send('Express + TypeScript Server');
+})
+
+
+app.listen(PORT , () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
